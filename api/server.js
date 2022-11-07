ff1108f9f2927f3093c3670cf98d14d17eb65906
Luxion Rob <aidaynhi8@gmail.com>
2022-11-07 11:47:14 +0700

    Prettier commit

---
+++ api/server.js
@@ -0,0 +1,30 @@
+// server.js
+const path = require('path')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use('/api', router)
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log('JSON Server is running in http://localhost:3000/api')
+})

5298b58cca60ca461e1458516ad46270feccb102
hoxxor <nhannt.gm@gmail.com>
2022-11-07 13:27:55 +0700

    update data api

--- api/seeder.js
+++ api/seeder.js
@@ -1,7 +1,13 @@
 const fs = require('fs')
+const users = require('./data/users.js')
+const products = require('./data/products.js')
 
 const start = () => {
-  
+  const db = {
+    users,
+    products,
+  }
+
   // write db object to db.json
   fs.writeFile('db.json', JSON.stringify(db), () => {
     console.log('Generate data successfully =))')

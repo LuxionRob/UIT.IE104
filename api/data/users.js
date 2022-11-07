a58e2e76006e947319f6f55fb5b31bbcca0f1930
hoxxor <nhannt.gm@gmail.com>
2022-11-07 13:29:18 +0700

    update data api

---
+++ api/data/users.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    userId: 1,
+    name: 'Nika Linh Lan',
+    address: 'Phòng 1, Tầng 82, Landmark 81',
+    phoneNumber: '0922338756',
+    gender: 'male',
+    email: 'nikalinhlan@nijigen.com',
+    aveUrl: '../frontend/src/images/ava/lankuto.png',
+    username: 'nikalinhlanvjppro123',
+    password: '123456789',
+  },
+  {
+    userId: 2,
+    name: 'Bao The Whale',
+    address: 'Phòng 2, Tầng 82, Landmark 81',
+    phoneNumber: '0922338757',
+    gender: 'animal',
+    email: 'whalewolfwolf@whale52hz.com',
+    aveUrl: '../frontend/src/images/ava/baoshort.png',
+    username: 'simpmoiminhem',
+    password: '123456789',
+  },
+]

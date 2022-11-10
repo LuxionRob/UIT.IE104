8637a92d8369c6f2ab74393b55a53b99ea58f42a
hoxxor <nhannt.gm@gmail.com>
2022-11-10 14:20:21 +0700

    update data product & user

--- server/data/product.js
+++ server/data/product.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     id: 1,
     type: 'milk tea',
-    name: "Lan's milk tea",
+    name: "Spirit's milk tea",
     price: 999.999,
     description: 'Đây là sữa của Tinh linh, rất ngon.',
   },

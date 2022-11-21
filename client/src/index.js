097883a7a57599a86561d48e901cb015bd75b4b4
nhannt <nhannt.gm@gmail.com>
2022-11-21 13:23:09 +0700

    [Repo] add tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './tailwind.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)

dc56d1fd334af6ecb1b37e5be68d45dc25bf7a12
nhannt <nhannt.gm@gmail.com>
2022-12-02 21:29:52 +0700

    [Repo] change tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', './public/*.html', './src/*.{jsx, js}'],
+  content: ['./src/**/*.{html,js,jsx}', './public/*.html', './src/*.{jsx, js}'],
   theme: {
     extend: {},
   },

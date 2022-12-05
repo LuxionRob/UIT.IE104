4923bb51295900d78857eda860e472e5444a788d
anhnguyet2310 <anhnguyet23102002@gmail.com>
2022-12-05 13:45:37 +0700

    Bla

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,47 +1,78 @@
 import React from 'react';
 import './Footer.css';
-import '../fonts/themify-icons-font/themify-icons/themify-icons.css';
+import {FaFacebook} from 'react-icons/fa'
+import {FaYoutube} from 'react-icons/fa'
+import {FaMapMarkerAlt} from 'react-icons/fa'
+import {FaPhoneAlt} from 'react-icons/fa'
+import {FaEnvelope} from 'react-icons/fa'
+
 const Footer = () => {
     return (
         <div className="main-footer">
             <div className="container">
-                <div className="row">
+                <ul className="row">
                     {/* column1 */}
-                    <div className="col">
+                    <li className="col">
                         <h4>Địa chỉ liên hệ</h4>
-                        <div className="Footer-address">
-                            <i className="ti-location-pin ti-icon"></i>
-                            5/15, đường 106, phường Tăng Nhơn Phú A, thành phố Thủ Đức
-                        </div>
-                    </div>
-                    {/* column2 */}
-                    <div className="col">
-                        <h4>Gmail</h4>
-                        <div className="Footer-gmail">
-                            <i className="ti-email ti-icon"></i>
-                            SeeTee@gmail.com
-                        </div>
-                    </div>
-                    {/* column3 */}
-                    <div className="col">
-                        <h4>Mạng xã hội</h4>
-                        <ul className="Footer-social">
+                        <ul className='Footer-icon'>
+                        <li>    
+                        <FaMapMarkerAlt color='#4d8b54'/>
+                        </li>  
+                        <li> 5/15, đường 106, phường Tăng Nhơn Phú A, thành phố Thủ Đức</li>
+                        </ul>
+
+                        <ul className='Footer-icon'>
                             <li>
-                                <a href="https://www.facebook.com/" className="Footer-icon">
-                                    <i className="ti-facebook ti-icon"></i>
-                                    Link facebook
+                                <a href="tel:035267814" >
+                                <FaPhoneAlt color='#4d8b54'/>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.youtube.com/watch?v=HYP9DLNsYwk" className="Footer-icon">
-                                    <i className="ti-youtube ti-icon"></i>
-                                    Link youtube
-                                </a>
+                                03-526-7814
+                            </li>
+                            </ul>
+                        
+                    </li>
+
+                    {/* column2 */}
+                    <li className="col">
+                        <h4>Gmail</h4>
+                        <ul className="Footer-icon">
+                            <li>
+                            <FaEnvelope color='#4d8b54'/>
+                            </li >  
+                            <li className='mail-to'> 
+                                info@fiveTea.com
+                                </li>
+                        </ul>
+                    </li>
+                    {/* column3 */}
+                    <li className="col">
+                        <h4>Liên hệ trực tiếp</h4>
+                    <ul className="Footer-icon">
+                        <li>
+                             <a href="https://www.youtube.com/watch?v=HYP9DLNsYwk">
+                            <FaFacebook color='#4d8b54'/> 
+                             </a>
+                        </li>
+                        
+                        <li>
+                        Link facebook
+                        </li>
+                    </ul>
+                        <ul className='Footer-icon'>
+                            <li>
+                              <a href="https://www.youtube.com/watch?v=HYP9DLNsYwk">
+                            <FaYoutube color='#4d8b54'/>
+                              </a>
+                            </li>
+                            <li>
+                            Link youtube
                             </li>
                         </ul>
-                    </div>
+                    </li>
                     {/* column4 */}
-                    <div className="col">
+                    <li className="col">
                         <h4>Chính sách</h4>
                         <ul className="Footer-policy">
                             <li>Hình thức thanh toán</li>
@@ -49,8 +80,8 @@ const Footer = () => {
                             <li>Bảo trì, bảo hành</li>
                             <li>Chính sách bảo mật</li>
                         </ul>
-                    </div>
-                </div>
+                    </li>
+                </ul>
                 <hr />
                 <div className="row2">
                     <p className="col-slogan">Chúng tôi biết bạn có nhiều sự lựa chọn,cảm ơn vì đã chọn chúng tôi</p>

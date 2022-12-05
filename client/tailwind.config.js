153385e3b995e88b6db8bbf84a896e5f35b1a107
Luxion Rob <aidaynhi8@gmail.com>
2022-12-05 08:18:02 +0700

    [Repo ] Change tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
+<<<<<<< Updated upstream
   content: ['./src/**/*.{html,js,jsx}', './public/*.html', './src/*.{jsx, js}'],
+=======
+  content: ['./src/**/*.{html,jsx, js}', './public/*.html', './src/*.{jsx, js}'],
+>>>>>>> Stashed changes
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4d8b54',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.input': {
+          border: 'solid 0.2rem #4d8b54',
+        },
+      })
+    }),
+  ],
 }

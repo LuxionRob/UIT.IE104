d2885c6e6ec4f276b89d64b481c3f771982c4670
Luxion Rob <aidaynhi8@gmail.com>
2022-12-05 14:34:17 +0700

    [Repo] Add loading component

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingSpinner() {
+  return (
+    <div className='spinner-container'>
+      <div className='animate-spin w-16 h-16 border border-gray-400 border-t-black rounded-full'></div>
+    </div>
+  )
+}

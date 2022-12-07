62ca873ec0a35b57c9699432108d0ab76734d592
Luxion <45875394+LuxionRob@users.noreply.github.com>
2022-12-07 09:50:33 +0700

    Update image.route.js

--- server/routes/image.route.js
+++ server/routes/image.route.js
@@ -5,7 +5,7 @@ const ImageController = require('../controllers/image.controller')
 router.get('/users/:id', ImageController.getUserImage)
 router.post('/users/:id', ImageController.updateUserImage)
 router.get('/products/:id', ImageController.getProductImage)
-router.get('/member-avatars/:id', ImageController.getMemberImage)
+router.get('/members/:id', ImageController.getMemberImage)
 router.get('/logo', ImageController.getLogo)
 
 module.exports = router

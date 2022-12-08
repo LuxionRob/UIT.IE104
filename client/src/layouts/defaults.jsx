99ef3ce260de76958d32732fabfb1bb319f5f1a9
Luxion <45875394+LuxionRob@users.noreply.github.com>
2022-12-08 15:16:12 +0700

    Update defaults.jsx

--- client/src/layouts/defaults.jsx
+++ client/src/layouts/defaults.jsx
@@ -7,7 +7,7 @@ const DefaultLayout = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <Outlet className='grow' />
+      <Outlet />
       <Footer />
     </div>
   )

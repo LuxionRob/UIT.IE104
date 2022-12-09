35c1c7229a6f932b26bf6a8346e3f86bbf18ab24
nhannt <nhannt.gm@gmail.com>
2022-12-09 18:15:17 +0700

    <(")

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Banner = ({ img }) => {
+const Banner = ({ imgSrc }) => {
   return (
     <div className='my-0 mx-auto max-w-[1100px]'>
       <Link className='' to='/products'>
-        <img className='my-0 mx-auto mt-10 rounded-xl' src='http://localhost:3003/api/images/banners/1' alt='Banner' />
+        <img className='my-0 mx-auto mt-10 rounded-xl' src={imgSrc} alt='Banner' />
       </Link>
     </div>
   )

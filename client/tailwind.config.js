cce64731dbc58a122d8d06e35b8537d271a1bb4b
nhannt <nhannt.gm@gmail.com>
2022-12-09 17:52:56 +0700

    [Repo] change to desktop first

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -22,6 +22,19 @@ module.exports = {
         '10/10': '100%',
       },
     },
+    screens: {
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+    },
   },
   plugins: [
     plugin(function ({ addBase, addComponents }) {

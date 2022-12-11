5ad8b0351cb13d36115aa4430e87e37f9ff866c4
nhannt <nhannt.gm@gmail.com>
2022-12-11 16:57:40 +0700

    fix button and something else

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,6 +4,8 @@ export function stringCut(string) {
     cutString = cutString.slice(0, 13) + '...'
   } else if (cutString.length > 22 && window.innerWidth < 1624) {
     cutString = cutString.slice(0, 20) + '...'
+  } else {
+    cutString = cutString.slice(0, 28) + '...'
   }
 
   return cutString
